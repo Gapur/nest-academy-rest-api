76558c92c1faf7269075f46a4b7add6fe45dfa18
Gapur Kassym <gapur.kassym@gmail.com>
2024-01-14 22:28:51 +0200

    using student repository to work with database

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { Student } from './entities/student.entity';
 
 @Injectable()
 export class StudentsService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
   create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+    const student = new Student();
+    student.firstName = createStudentDto.firstName;
+    student.lastName = createStudentDto.lastName;
+    student.email = createStudentDto.email;
+    student.address = createStudentDto.address;
+    return this.studentRepository.save(student);
   }
 
   findAll() {
-    return `This action returns all students`;
+    return this.studentRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} student`;
+    return this.studentRepository.findOneBy({ id });
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+  async update(id: number, updateStudentDto: UpdateStudentDto) {
+    const studentById = await this.studentRepository.findOneBy({ id });
+    studentById.firstName = updateStudentDto.firstName;
+    studentById.lastName = updateStudentDto.lastName;
+    studentById.email = updateStudentDto.email;
+    studentById.address = updateStudentDto.address;
+    return this.studentRepository.save(studentById);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} student`;
+    return this.studentRepository.delete(id);
   }
 }
