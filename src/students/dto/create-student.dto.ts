aa3d615f33f8f8f0563330d05876ddff233c8b19
Gapur Kassym <gapur.kassym@gmail.com>
2024-01-14 21:44:41 +0200

    update student dto with validation

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,1 +1,22 @@
-export class CreateStudentDto {}
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateStudentDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  address: string;
+}
